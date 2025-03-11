14048ad627d51bf69c81e8b9e29fc65b9f8d030e
abdullah107189 <abdullah107189@gmail.com>
2025-03-11 22:50:25 +0600

    set up react router dom, tailwind css and vite

---
+++ src/Router/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../page/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);

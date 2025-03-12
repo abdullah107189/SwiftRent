e30cec2ca4ac1b660fcfe8a20022340c6ad7faab
IBRAHIM <ibrahim.webdev10@gmail.com>
2025-03-12 23:03:29 +0300

    parallax add

--- src/page/Contact/ContactFrom.jsx
+++ src/page/Contact/ContactFrom.jsx
@@ -1,29 +1,21 @@
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 const ContactForm = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000, once: true });
-  }, []);
-
   return (
     <section className="max-w-6xl mx-auto px-6 sm:px-12 py-16 grid grid-cols-1 md:grid-cols-2 gap-10">
       {/* Left Side - Get in Touch */}
-      <div className="space-y-6" data-aos="fade-right">
+      <div className="space-y-6">
         <h2 className="text-3xl font-bold text-[#F5B754]">Get in Touch</h2>
-        <form className="space-y-4 ">
+        <form className="space-y-4">
           <div className="flex flex-wrap gap-4">
             <input
               type="text"
-              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-l-full rounded-r-full "
+              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-full"
               placeholder="Your Name *"
               required
             />
 
             <input
               type="email"
-              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-l-full rounded-r-full "
+              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-full"
               placeholder="Your Email *"
               required
             />
@@ -32,13 +24,13 @@ const ContactForm = () => {
           <div className="flex flex-wrap gap-4">
             <input
               type="text"
-              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-l-full rounded-r-full "
+              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-full"
               required
               placeholder="Your Number *"
             />
             <input
               type="text"
-              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-l-full rounded-r-full "
+              className="w-full sm:w-60 p-3 block bg-[#222222] text-white border border-gray-600 focus:ring focus:ring-[#F5B754] rounded-full"
               required
               placeholder="Subject *"
             />
@@ -59,20 +51,18 @@ const ContactForm = () => {
         </form>
       </div>
 
-      {/* Right Side - Contact Form */}
-      <div
-        className="bg-[#1C1818] p-8 rounded-lg shadow-lg"
-        data-aos="fade-left"
-      >
+      {/* Right Side - Google Maps Embed */}
+      <div className=" p-2">
         <div>
-          <label className="block text-white mb-1">Location</label>
-          <input
-            type="text"
-            className="w-full p-3 rounded bg-gray-800 text-white border border-gray-600 focus:ring focus:ring-[#F5B754]"
-          />
-          this is contact Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Tempore fugit doloribus sit, ab laudantium facilis vel quos odit
-          fuga quam?
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14602.680856404126!2d90.39540585322665!3d23.794755208239323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c70c15ea1de1%3A0x97856381e88fb311!2sBanani%2C%20Dhaka%2C%20Bangladesh!5e0!3m2!1sen!2ssa!4v1741807295587!5m2!1sen!2ssa"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </section>

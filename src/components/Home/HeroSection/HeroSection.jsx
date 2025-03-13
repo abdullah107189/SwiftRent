b07ec915b5af4f1f7602a08088276198eec68d52
abdullah107189 <abdullah107189@gmail.com>
2025-03-13 22:14:21 +0600

    Create horo section with content and style

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -9,8 +9,51 @@ import { Link } from "react-router-dom";
 import backgroundImage1 from "../../../assets/heroSection/1.jpg";
 import backgroundImage2 from "../../../assets/heroSection/2.jpg";
 import backgroundImage3 from "../../../assets/heroSection/3.jpg";
+import {
+  FaCarSide,
+  FaUsers,
+  FaCogs,
+  FaSuitcaseRolling,
+  FaUser,
+} from "react-icons/fa";
 
 const HeroSection = () => {
+  const specs1 = [
+    { icon: <FaCarSide />, label: "Doors", value: 2, border: true },
+    { icon: <FaUsers />, label: "Passengers", value: 2, border: true },
+    { icon: <FaCogs />, label: "Transmission", value: "Auto", border: true },
+    {
+      icon: <FaSuitcaseRolling />,
+      label: "Luggage",
+      value: "1 Bag",
+      border: true,
+    },
+    { icon: <FaUser />, label: "Age", value: 27 },
+  ];
+  const specs2 = [
+    { icon: <FaCarSide />, label: "Doors", value: 4, border: true },
+    { icon: <FaUsers />, label: "Passengers", value: 4, border: true },
+    { icon: <FaCogs />, label: "Transmission", value: "Auto", border: true },
+    {
+      icon: <FaSuitcaseRolling />,
+      label: "Luggage",
+      value: "2 Bag",
+      border: true,
+    },
+    { icon: <FaUser />, label: "Age", value: 25 },
+  ];
+  const specs3 = [
+    { icon: <FaCarSide />, label: "Doors", value: 2, border: true },
+    { icon: <FaUsers />, label: "Passengers", value: 4, border: true },
+    { icon: <FaCogs />, label: "Transmission", value: "Auto", border: true },
+    {
+      icon: <FaSuitcaseRolling />,
+      label: "Luggage",
+      value: "1 Bag",
+      border: true,
+    },
+    { icon: <FaUser />, label: "Age", value: 30 },
+  ];
   return (
     <div className="hFull flex items-center justify-center">
       <Swiper
@@ -32,6 +75,7 @@ const HeroSection = () => {
         modules={[Pagination, Autoplay]}
         className="mySwiper "
       >
+        {/* first slider  */}
         <SwiperSlide>
           <div
             className="hFull bg-cover bg-center"
@@ -39,10 +83,69 @@ const HeroSection = () => {
               backgroundImage: `url(${backgroundImage1})`,
             }}
           >
-            <h1>hello</h1>
+            <div className="mxw flex items-center justify-between">
+              {/* left side content  */}
+              <div className=" h-screen flex items-center">
+                {/* Background Image (Replace with your Bugatti image) */}
+                <div
+                  className="absolute inset-0 bg-cover bg-center"
+                  style={{ backgroundImage: `url('/bugatti-mistral.jpg')` }}
+                >
+                  {/* Dark overlay */}
+                  <div className="absolute inset-0 bg-black opacity-50"></div>
+                </div>
+
+                <div className="relative z-10 text-white p-8">
+                  <div className="uppercase text-sm mb-2 tracking-[1rem] flex gap-5 items-center orange">
+                    <p className="w-2 h-2 rounded-full bgOrange"></p>
+                    Economy
+                  </div>
+                  <h1 className="text-6xl font-bold mb-4">
+                    Bugatti Mistral W16
+                  </h1>
+                  <p className="text-lg mb-6">
+                    Reserve now and get the best offer
+                    <span className="font-bold orange ml-5 text-2xl">$750</span>
+                    /DAY
+                  </p>
+                  <div className="flex gap-5 ">
+                    <button className="fillBtn">
+                      View Details
+                      <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+
+                    <button className="outlineBtn">
+                      Rent Now <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* right side content  */}
+              <div className="bg-[#1b1b1b] rounded-2xl p-10 w-[400px] z-10">
+                {specs1.map((spec, index) => (
+                  <div
+                    key={index}
+                    className={`${
+                      spec.border == true
+                        ? " border-b border-[#999]/10"
+                        : "border-none"
+                    } flex items-center py-3 justify-between border-b border-[#999]/10 last:mb-0`}
+                  >
+                    <div className="flex items-center">
+                      <span className="bg-[#f5b754]/10 p-2 orange rounded-full mr-3 text-lg">
+                        {spec.icon}
+                      </span>
+                      <span className="tBlack">{spec.label}</span>
+                    </div>
+                    <span className="text-white">{spec.value}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </SwiperSlide>
 
+        {/* second slider  */}
         <SwiperSlide>
           <div
             className="hFull bg-cover bg-center"
@@ -50,10 +153,67 @@ const HeroSection = () => {
               backgroundImage: `url(${backgroundImage2})`,
             }}
           >
-            <h1>hello</h1>
+            <div className="mxw flex items-center justify-between">
+              {/* left side content  */}
+              <div className=" h-screen flex items-center">
+                {/* Background Image (Replace with your Bugatti image) */}
+                <div
+                  className="absolute inset-0 bg-cover bg-center"
+                  style={{ backgroundImage: `url('/bugatti-mistral.jpg')` }}
+                >
+                  {/* Dark overlay */}
+                  <div className="absolute inset-0 bg-black opacity-50"></div>
+                </div>
+
+                <div className="relative z-10 text-white p-8">
+                  <div className="uppercase text-sm mb-2 tracking-[1rem] flex gap-5 items-center orange">
+                    <p className="w-2 h-2 rounded-full bgOrange"></p>
+                    Economy
+                  </div>
+                  <h1 className="text-6xl font-bold mb-4">Bentley Bentayga</h1>
+                  <p className="text-lg mb-6">
+                    Reserve now and get the best offer
+                    <span className="font-bold orange ml-5 text-2xl">$750</span>
+                    /DAY
+                  </p>
+                  <div className="flex gap-5 ">
+                    <button className="fillBtn">
+                      View Details
+                      <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+
+                    <button className="outlineBtn">
+                      Rent Now <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* right side content  */}
+              <div className="bg-[#1b1b1b] rounded-2xl p-10 w-[400px] z-10">
+                {specs2.map((spec, index) => (
+                  <div
+                    key={index}
+                    className={`${
+                      spec.border == true
+                        ? " border-b border-[#999]/10"
+                        : "border-none"
+                    } flex items-center py-3 justify-between border-b border-[#999]/10 last:mb-0`}
+                  >
+                    <div className="flex items-center">
+                      <span className="bg-[#f5b754]/10 p-2 orange rounded-full mr-3 text-lg">
+                        {spec.icon}
+                      </span>
+                      <span className="tBlack">{spec.label}</span>
+                    </div>
+                    <span className="text-white">{spec.value}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </SwiperSlide>
 
+        {/* third slider  */}
         <SwiperSlide>
           <div
             className="hFull bg-cover bg-center"
@@ -61,7 +221,65 @@ const HeroSection = () => {
               backgroundImage: `url(${backgroundImage3})`,
             }}
           >
-            <h1>hello</h1>
+            <div className="mxw flex items-center justify-between">
+              {/* left side content  */}
+              <div className=" h-screen flex items-center">
+                {/* Background Image (Replace with your Bugatti image) */}
+                <div
+                  className="absolute inset-0 bg-cover bg-center"
+                  style={{ backgroundImage: `url('/bugatti-mistral.jpg')` }}
+                >
+                  {/* Dark overlay */}
+                  <div className="absolute inset-0 bg-black opacity-50"></div>
+                </div>
+
+                <div className="relative z-10 text-white p-8">
+                  <div className="uppercase text-sm mb-2 tracking-[1rem] flex gap-5 items-center orange">
+                    <p className="w-2 h-2 rounded-full bgOrange"></p>
+                    Economy
+                  </div>
+                  <h1 className="text-6xl font-bold mb-4">
+                    Rolls Royce Cullinan
+                  </h1>
+                  <p className="text-lg mb-6">
+                    Reserve now and get the best offer
+                    <span className="font-bold orange ml-5 text-2xl">$750</span>
+                    /DAY
+                  </p>
+                  <div className="flex gap-5 ">
+                    <button className="fillBtn">
+                      View Details
+                      <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+
+                    <button className="outlineBtn">
+                      Rent Now <span className="ml-2 -rotate-45">&#8594;</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* right side content  */}
+              <div className="bg-[#1b1b1b] rounded-2xl p-10 w-[400px] z-10">
+                {specs3.map((spec, index) => (
+                  <div
+                    key={index}
+                    className={`${
+                      spec.border == true
+                        ? " border-b border-[#999]/10"
+                        : "border-none"
+                    } flex items-center py-3 justify-between border-b border-[#999]/10 last:mb-0`}
+                  >
+                    <div className="flex items-center">
+                      <span className="bg-[#f5b754]/10 p-2 orange rounded-full mr-3 text-lg">
+                        {spec.icon}
+                      </span>
+                      <span className="tBlack">{spec.label}</span>
+                    </div>
+                    <span className="text-white">{spec.value}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>

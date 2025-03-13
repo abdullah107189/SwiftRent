409870723ace9ccd1384c3284ea91ed8bb91f6d9
Your Name <khalidsaifullaha3@gmail.com>
2025-03-13 11:10:46 +0600

    container size change

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/shared/Navbar';
-import Footer from '../components/shared/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="max-w-screen-xl mx-auto">
+      <div className=" ">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>

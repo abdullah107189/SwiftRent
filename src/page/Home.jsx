6a4caa55ce5c81793743e93307b7718b253748bb
siam114 <smsiamislam598@gmail.com>
2025-03-13 16:59:18 +0600

    create share component section header

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/Home/HeroSection/HeroSection";
+import RentalCarSection from "./Home/RentalCarSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection></HeroSection>
+      <RentalCarSection/>
     </div>
   );
 };

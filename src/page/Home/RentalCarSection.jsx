75f32bc9df8b985bf633d6176527537815fcf5a4
siam114 <smsiamislam598@gmail.com>
2025-03-13 16:59:18 +0600

    create share component section header

---
+++ src/page/Home/RentalCarSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionHeader from '../../components/shared/SectionHeader';
+
+const RentalCarSection = () => {
+    return (
+        <div>
+        <SectionHeader title="Categories" subtitle="Rental Car Types" />
+        {/* Other content */}
+      </div>
+    );
+};
+
+export default RentalCarSection;

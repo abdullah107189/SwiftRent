93b8d1c620574052c64ff6bf715c7eb70a1d5ddd
abdullah107189 <abdullah107189@gmail.com>
2025-03-14 08:56:54 +0600

    create page header and style css better

--- src/page/Contact/Contact.jsx
+++ src/page/Contact/Contact.jsx
@@ -1,13 +1,19 @@
-import Card from './Card';
-import Carousel from './Carousel';
-import CarParallax from './CarParallax';
-import ContactBanner from './ContactBanner';
-import ContactForm from './ContactFrom';
+import PageHeader from "../../components/shared/PageHeader";
+import Card from "./Card";
+import Carousel from "./Carousel";
+import CarParallax from "./CarParallax";
+import ContactBanner from "./ContactBanner";
+import ContactForm from "./ContactFrom";
 
 const Contact = () => {
   return (
     <div>
-      <ContactBanner />
+      <PageHeader
+        subTitle={"Get in touch"}
+        titleWhite={"Contact"}
+        titleOrange={"Us"}
+        line={false}
+      ></PageHeader>
       <div className="mxw">
         <Card />
         <ContactForm />

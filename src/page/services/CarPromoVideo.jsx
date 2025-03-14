8a95318698ed5db17c6e718a677447b5af99a1d2
Your Name <khalidsaifullaha3@gmail.com>
2025-03-14 10:57:32 +0600

    car promo video added

---
+++ src/page/services/CarPromoVideo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CarPromoVideo from "../../assets/heroSection/1.jpg";
+
+function CarPromoVideo() {
+  return <div>CarPromoVideo</div>;
+}
+
+export default CarPromoVideo;

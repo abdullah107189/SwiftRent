a97e558a6521e4b5ee9cc8baf89ec4e3cac732cf
programming-ibrahim <freelanceribrahim39@gmail.com>
2025-03-21 23:41:55 +0300

    profile update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,57 +1,40 @@
-import { useState } from 'react';
-
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import avatar from '../../assets/default-avatar.png';
+import { useDispatch, useSelector } from 'react-redux';
 const Header = ({ title }) => {
-  const [open, setOpen] = useState(false);
+  const { user } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  console.log(dispatch);
 
   return (
     <header className=" px-2 bg-opacity-50 backdrop-blur-lg border-b border-gray-700 m sticky top-0 z-50 bolck ">
       <div className="flex justify-between items-center w-full">
         <h1 className="text-3xl mb-3 font-semibold">{title}</h1>
 
-        <div className="relative md:block">
-          <button
-            type="button"
-            className="overflow-hidden rounded-full border border-gray-300 shadow-inner"
-            onClick={() => setOpen(!open)}
-          >
-            <span className="sr-only">Toggle dashboard menu</span>
-            <img
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Profile"
-              className="w-10 h-10 object-cover rounded-full"
-            />
-          </button>
-
-          {open && (
-            <div
-              className="absolute right-0 z-10 mt-0.5 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-lg"
-              role="menu"
+        <div className="relative flex gap-4 md:block">
+          <div className="flex gap-4 text-center  items-center">
+            <a
+              href="#"
+              class="relative inline-flex items-center justify-center w-10 h-10  text-white rounded-full bg-[#f5b754]"
             >
-              <div className="p-2">
-                <a
-                  href="#"
-                  className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                  role="menuitem"
-                >
-                  My profile
-                </a>
-                <a
-                  href="#"
-                  className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                  role="menuitem"
-                >
-                  Billing summary
-                </a>
-                <a
-                  href="#"
-                  className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                  role="menuitem"
-                >
-                  Team settings
-                </a>
-              </div>
-            </div>
-          )}
+              <IoIosNotificationsOutline className="text-2xl sm:text-3xl text-white" />
+              <span class="absolute -top-1.5 -right-1.5 inline-flex items-center justify-center gap-1 rounded-full border-2 border-[#f5b754] bg-[#222121] px-1.5 text-sm text-[#f5b754]">
+                7
+              </span>
+            </a>
+            <Link
+              to="/dashboard/profile"
+              className="overflow-hidden rounded-full border border-gray-300 shadow-inner"
+            >
+              <span className="sr-only">Toggle dashboard menu</span>
+              <img
+                src={user?.photoURL || avatar}
+                alt="Profile"
+                className="w-10 h-10 object-cover rounded-full cursor-pointer"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </header>

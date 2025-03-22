fc9d724e6c626d398eb8670a6389ea4935690954
abdullah107189 <abdullah107189@gmail.com>
2025-03-22 21:44:48 +0600

    fixed deploying problem using vercel.json

--- src/hooks/useGetCars.js
+++ src/hooks/useGetCars.js
@@ -2,19 +2,36 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosPublic from "./useAxiosPublic";
 
-const useGetCars = () => {
+const useGetCars = (
+  search = {},
+  filterBrand = {},
+  priceRange = {},
+  carType = {},
+  fuelType = {},
+  sortOption = {}
+) => {
   const axiosPublic = useAxiosPublic();
+
   const {
     data: cars = [],
     isLoading,
     refetch,
   } = useQuery({
-    queryKey: ["cars"],
+    queryKey: [
+      "cars",
+      search,
+      filterBrand,
+      priceRange,
+      carType,
+      fuelType,
+      sortOption,
+    ],
     queryFn: async () => {
       const { data } = await axiosPublic.get("/cars");
       return data;
     },
   });
+
   return { cars, refetch, isLoading };
 };
 

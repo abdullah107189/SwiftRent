8f74723b41ed68005f44fd51866e040cc45a18e2
Rahul <rahulwp24@gmail.com>
2025-03-24 09:45:56 +0600

    detail page data load

--- src/components/common/CarDetails/CarDetails.jsx
+++ src/components/common/CarDetails/CarDetails.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import detaisImg from "/src/assets/details/details.png";
-
 import PageHeader from "../../shared/PageHeader";
 import DetailsCard from "./DetailsCard";
 
 export default function CarDetails() {
+  const { id } = useParams(); 
+  const [car, setCar] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  console.log(id)
+  useEffect(() => {
+    const fetchCarDetails = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/cars/${id}`); 
+        const data = await response.json();
+        setCar(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching car details:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchCarDetails();
+  }, [id]);
+
+  if (loading) {
+    return <p className="text-center text-white">Loading...</p>;
+  }
+
+  if (!car) {
+    return <p className="text-center text-red-500">Car not found!</p>;
+  }
+
   return (
-    <div className="">
+    <div>
       <PageHeader
         subTitle="Available Cars"
         titleWhite="Car"
@@ -14,7 +44,8 @@ export default function CarDetails() {
         image={detaisImg}
       />
       <div className="mxw">
-        <DetailsCard />
+        <div>h{car.name  }</div>
+        <DetailsCard car={car} />
       </div>
     </div>
   );

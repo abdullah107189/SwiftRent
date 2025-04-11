81c7986c925ecfef93fcc2c558fb794ba5d8ef4d
IBRAHIM <ibrahim.webdev10@gmail.com>
2025-04-11 08:30:01 +0300

    admin all roters page completed

--- src/DeahBoardPage/Admin/Users/UsersTable .jsx
+++ src/DeahBoardPage/Admin/Users/UsersTable .jsx
@@ -1,73 +1,70 @@
-import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { Search } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
-import { useSelector } from "react-redux";
-import { FaRegTrashAlt } from "react-icons/fa";
-import Spinner from "../../components/Spinner";
-import Swal from "sweetalert2";
-const UsersTable = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const { user } = useSelector((state) => state.auth);
-  const axiosSecure = useAxiosSecure();
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { Search } from 'lucide-react';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
-  // Fetch users data using React Query
+import { FaRegTrashAlt } from 'react-icons/fa';
+import Spinner from '../../../components/Spinner';
+import Swal from 'sweetalert2';
+const UsersTable = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const axiosSecure = useAxiosSecure();
+  const role = 'customer';
   const {
     data: users = [],
     isLoading,
     refetch,
   } = useQuery({
-    queryKey: ["users", user?.email],
+    queryKey: ['users', role],
     queryFn: async () => {
-      const { data } = await axiosSecure(`/all-user/${user?.email}`);
+      const { data } = await axiosSecure(`/customers/${role}`);
       return data;
     },
   });
 
-  // Update filteredUsers when users change
   useEffect(() => {
     setFilteredUsers(users);
   }, [users]);
 
   // Search Functionality
-  const handleSearch = (e) => {
+  const handleSearch = e => {
     const term = e.target.value.toLowerCase();
     setSearchTerm(term);
     const filtered = users.filter(
-      (u) =>
+      u =>
         u.name.toLowerCase().includes(term) ||
         u.email.toLowerCase().includes(term)
     );
     setFilteredUsers(filtered);
   };
 
-  const handelUserDelete = async (id) => {
+  const handelUserDelete = async id => {
     Swal.fire({
-      title: "Are you sure?",
-      text: "User  will be deleted",
-      icon: "warning",
+      title: 'Are you sure?',
+      text: 'User  will be deleted',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then(async (result) => {
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(async result => {
       if (result.isConfirmed) {
         try {
           await axiosSecure.delete(`/user-delete/${id}`);
           refetch();
           Swal.fire({
-            title: "Deleted!",
-            text: "User has been deleted.",
-            icon: "success",
+            title: 'Deleted!',
+            text: 'Customers has been deleted.',
+            icon: 'success',
           });
         } catch (error) {
           console.error(error);
           Swal.fire({
-            title: "Error!",
-            text: "Something went wrong.",
-            icon: "error",
+            title: 'Error!',
+            text: 'Something went wrong.',
+            icon: 'error',
           });
         }
       }
@@ -78,18 +75,18 @@ const UsersTable = () => {
 
   return (
     <motion.div
-      className="  shadow-lg rounded-xl p-6 border border-gray-700"
+      className="  shadow-lg rounded-xl p-6 border border-gray-700 p"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.2 }}
     >
       <div className="flex justify-between items-center mb-6">
-        <h2 className="text-xl font-semibold text-gray-100">Users</h2>
+        <h2 className="text-xl font-semibold text-gray-100">Customers Lists</h2>
         <div className="relative">
           <input
             type="text"
             placeholder="Search users..."
-            className="bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#f5b754]"
+            className="bg-[#f5b754]/10 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-500"
             value={searchTerm}
             onChange={handleSearch}
           />
@@ -101,7 +98,7 @@ const UsersTable = () => {
         <table className="min-w-full divide-y divide-gray-700">
           <thead>
             <tr>
-              {["Name", "Email", "Role", "Status", "Actions"].map((heading) => (
+              {['Name', 'Email', 'Phone', 'Status', 'Actions'].map(heading => (
                 <th
                   key={heading}
                   className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
@@ -113,7 +110,7 @@ const UsersTable = () => {
           </thead>
 
           <tbody className="divide-y divide-gray-700">
-            {filteredUsers.map((u) => (
+            {filteredUsers.map(u => (
               <motion.tr
                 key={u._id}
                 initial={{ opacity: 0 }}
@@ -122,36 +119,38 @@ const UsersTable = () => {
               >
                 <td className="px-6 py-4 whitespace-nowrap">
                   <div className="flex items-center">
-                    <div className="flex-shrink-0 h-10 w-10">
-                      <div className="h-10 w-10 rounded-full bg-gradient-to-r from-purple-400 to-blue-500 flex items-center justify-center text-white font-semibold">
-                        {u.name.charAt(0)}
-                      </div>
-                    </div>
                     <div className="ml-4">
                       <div className="text-sm font-medium text-gray-100">
-                        {u.name}
+                        <img
+                          src={u?.userInfo?.photoURL}
+                          alt="user"
+                          className="w-10 h-10 rounded-full border border-yellow-800"
+                        />
+                        {u?.userInfo?.name}
                       </div>
                     </div>
                   </div>
                 </td>
 
                 <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-300">{u.email}</div>
+                  <div className="text-sm text-gray-300">
+                    {u?.userInfo?.email}
+                  </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
                   <span className="px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-[#f5b754] text-whaite cursor-pointer">
-                    {u.role}
+                    {'+8801703500000'}
                   </span>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
                   <span
                     className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
                       u.isActive
-                        ? "bg-green-800 text-green-100"
-                        : "bg-red-800 text-red-100"
+                        ? 'bg-green-800 text-green-100'
+                        : 'bg-red-800 text-red-100'
                     }`}
                   >
-                    {u.isActive ? "Active" : "Inactive"}
+                    {u.isActive ? 'Active' : 'Inactive'}
                   </span>
                 </td>
 

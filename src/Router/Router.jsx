3321a50f62fdba0907471b8769f875e042f93f9c
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-04-11 18:24:34 +0600

    Solve error

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -41,7 +41,6 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    // errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",

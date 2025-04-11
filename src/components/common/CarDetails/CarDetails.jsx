93c396daf363da61b15a4bfdc9c014d6a686b7e9
Your Name <khalidsaifullaha3@gmail.com>
2025-04-11 14:48:05 +0600

    Added Helmet to all pages for improved document head management

--- src/components/common/CarDetails/CarDetails.jsx
+++ src/components/common/CarDetails/CarDetails.jsx
@@ -1,13 +1,24 @@
-import DetailsContent from './DetailsContent';
-import Car_Details from './Car_Details';
+import DetailsContent from "./DetailsContent";
+import Car_Details from "./Car_Details";
+import { Helmet } from "react-helmet-async";
 
 const CarDetails = () => {
   return (
-    <div className="mt-10">
-      <Car_Details />
+    <>
+      {" "}
+      <Helmet>
+        <title>Car Details | SwiftRent</title>
+        <meta
+          name="description"
+          content="View detailed information about cars available for rent on SwiftRent. Get the best car options tailored to your needs."
+        />
+      </Helmet>
+      <div className="mt-10">
+        <Car_Details />
 
-      <DetailsContent />
-    </div>
+        <DetailsContent />
+      </div>
+    </>
   );
 };
 

334ddff5eae251749e8ba3782ec26672de4ba1e8
Your Name <khalidsaifullaha3@gmail.com>
2025-04-16 22:52:45 +0600

    customer message added in dashbord

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -36,6 +36,7 @@ import ManageDrivers from "../DeahBoardPage/Admin/ManageDrivers";
 import BillingPage from "../DeahBoardPage/Admin/BillingPage";
 import Updatecar from "../DeahBoardPage/Admin/ManageCars/Updatecar";
 import AvailableTrips from "../DeahBoardPage/Driver/AvailableTrips";
+import LiveChat from "../components/liveChat/LiveChat";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -95,6 +96,7 @@ export const router = createBrowserRouter([
 
       { path: "customers-manage", element: <UsersPage /> },
       { path: "manage-drivers", element: <ManageDrivers /> },
+      { path: "live-chat", element: <LiveChat /> },
       { path: "billing", element: <BillingPage /> },
       { path: "order", element: <OrdersPage /> },
       { path: "settings", element: <SettingsPage /> },

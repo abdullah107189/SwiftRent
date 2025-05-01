94f2e238c37eea2cad8260e0a2d01b94f67fde45
Rahul <rahulwp24@gmail.com>
2025-05-01 09:39:38 +0600

    car types daynamic

--- src/components/shared/card/NameCard.jsx
+++ src/components/shared/card/NameCard.jsx
@@ -6,7 +6,7 @@ const NameCard = ({ name, image, number, onClick }) => {
       onClick={onClick}
       className="relative h-[300px] w-full md:h-[350px] rounded-3xl overflow-hidden group"
     >
-      {/* content  */}
+      {/* content  car */}
       <div className="relative w-full h-full rounded-xl overflow-hidden transition-transform duration-300 hover:scale-100">
         <img
           src={image}
